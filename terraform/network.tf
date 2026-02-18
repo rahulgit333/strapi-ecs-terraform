f1a9e2dbf591c65ad2eb0c75e2e0ced37886d423
rahul danodia <rahuldanodia07@gmail.com>
2026-02-18 08:06:05 +0530

    service and network changed

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "ecs" {
-  name = "ecs-sg"
+  name = "ecs-sg-v2"
   vpc_id = var.vpc_id
 
   ingress {
--- terraform/service.tf
+++ terraform/service.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_service" "strapi" {
-  name = "strapi-service"
+  name = "strapi-service-v2"
   cluster = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.strapi.arn
   desired_count = 1
